c4c7e8b9da3030fe178923840cadc98f9f814b74
G2-Games <ke0bhogsg@gmail.com>
2024-05-22 02:05:20 -0500

    Removed unused font things

--- utils/src/main.rs
+++ utils/src/main.rs
@@ -7,20 +7,20 @@ use cz::{
 
 fn main() {
     // Open the desired PNG
-    let new_bitmap = image::open("CGGALLERY_CH01_003.png")
+    let new_bitmap = image::open("mio.png")
         .unwrap()
         .to_rgba8();
 
-    let mut gallery_cz = DynamicCz::open("CGGALLERY_CH01_003").unwrap();
+    let mut gallery_cz = DynamicCz::open("166.cz3").unwrap();
 
     gallery_cz.set_bitmap(new_bitmap.into_vec());
-    gallery_cz.header_mut().set_depth(32);
-    gallery_cz.header_mut().set_version(CzVersion::CZ0);
-    gallery_cz.save_as_cz("CGGALLERY_CH01_003-MODIFIED").unwrap();
+    gallery_cz.header_mut().set_depth(8);
+    gallery_cz.header_mut().set_version(CzVersion::CZ3);
+    gallery_cz.save_as_cz("mio_modified-smallchunks.cz3").unwrap();
 
     // Open that same CZ3 again to test decoding
-    let cz_image_test = DynamicCz::open("CGGALLERY_CH01_003-MODIFIED").unwrap();
+    let cz_image_test = DynamicCz::open("mio_modified-smallchunks.cz3").unwrap();
 
     // Save the newly decoded CZ3 as another PNG as a test
-    cz_image_test.save_as_png("CGGALLERY_CH01_003-MODIFIED.png").unwrap();
+    cz_image_test.save_as_png("mio_modified.png").unwrap();
 }
